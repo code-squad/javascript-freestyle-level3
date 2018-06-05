798def7bdf89ea00d2983e5dada8cf8290ead5a5
Dongwoo Lee <likedemian@me.com>
2018-06-05 23:56:32 +0900

    add: features to swith categories list

--- src/js/app.js
+++ src/js/app.js
@@ -5,39 +5,75 @@ const template = (templateid, data) => {
 
 
 
+
 const showItems = (val, templateId) => {
-  return val.reduce((acc, curr) => acc += template(templateId, curr), '')
+  return val.reduce((acc, curr) => acc += template(templateId, curr), '');
 }
 
 
 
-const mainCategoriesTmpl = (function () {
-  let categoriesFilters = document.querySelector('.header__gnb__left-nav__categories-list__filter');
+
+
+const categoriesTemplate = (function () {
   const _makeFilters = () => {
-    return categoriesFilters.innerHTML += showItems(mainCategories, 'header__gnb__left-nav__categories-list__template')
+    let filters = document.querySelector('.header__gnb__left-nav__categories-list__filter');
+    return filters.innerHTML += showItems(mainCategories, 'header__gnb__left-nav__categories-list__template');
+  }
+
+  const _makeFilterLists = () => {
+    let filterLists = document.querySelector('.header__gnb__left-nav__categories-list__filtered-list');
+    return filterLists.innerHTML += showItems(categoriesLists[0], 'header__gnb__left-nav__categories-list__filter-list__template');
   }
   return {
-    getFilters() {
-      return _makeFilters();
+    getFilter() {
+      return _makeFilters() && _makeFilterLists();
     }
   }
 })();
+categoriesTemplate.getFilter();
 
 
 
-const subCategoriesTmpl = (function () {
-  let categoriesFilterList = document.querySelector('.header__gnb__left-nav__categories-list__filtered-list');
-  const _makeFilterItems = () => {
-    return categoriesFilterList.innerHTML += showItems(categories.characteristic, 'header__gnb__left-nav__categories-list__filter-list__template')
-  }
-  return {
-    getFilterItems() {
-      return _makeFilterItems();
-    }
-  }
-})();
 
 
 
-mainCategoriesTmpl.getFilters();
-subCategoriesTmpl.getFilterItems();
+const categoriesLink = document.querySelector('.header__gnb__left-nav__categories-link');
+const categoriesBox = document.querySelector('.header__gnb__left-nav__categories-list');
+const categoriesNav = document.querySelector('.header__gnb__left-nav__categories');
+
+categoriesLink.addEventListener('pointerover', e => {
+  categoriesBox.classList.add('active')
+})
+
+categoriesBox.addEventListener('pointerleave', e => {
+  categoriesBox.classList.remove('active')
+})
+
+categoriesNav.addEventListener('pointerleave', e => {
+  categoriesBox.classList.remove('active')
+})
+
+
+
+const switchFilter = () => {
+  const categoriesFilterBtns = document.querySelectorAll('.header__gnb__left-nav__categories-list__filter__item');
+  categoriesFilterBtns.forEach((elem, idx) => {
+    elem.addEventListener('click', e => {
+      let filterLists = document.querySelector('.header__gnb__left-nav__categories-list__filtered-list');
+      filterLists.innerHTML =
+        `
+        <template id="header__gnb__left-nav__categories-list__filter-list__template">
+          <li class="header__gnb__left-nav__categories-list__filtered__item">
+            <a class="filtered__item__link" href="#">{{name}}</a>
+          </li>
+        </template>
+        `
+      return filterLists.innerHTML += showItems(categoriesLists[idx], 'header__gnb__left-nav__categories-list__filter-list__template');
+    })
+  })
+}
+
+switchFilter();
+
+
+
