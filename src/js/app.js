be70b3bb00a071f097895a15df4461aba6e77c9b
Dongwoo Lee <likedemian@me.com>
2018-06-13 11:39:06 +0900

    Fix: Categories methods to Module Prototype Methods

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,12 @@
-const $on = (selector, type, callback) => {
-  selector.addEventListener(type, callback)
-}
+import { $on, $qs, $qsa } from './helper.js';
 
 const template = (templateid, data) => {
-  return document.getElementById(templateid).innerHTML
-    .replace(/{{(\w*)}}/g, (match, key) => data.hasOwnProperty(key) ? data[key] : '');
+  return document.getElementById(templateid).innerHTML.replace(/{{(\w*)}}/g, (match, key) => data.hasOwnProperty(key) ? data[key] : '');
 }
-
 
 const showItems = (val, templateId) => {
   return val.reduce((acc, curr) => acc += template(templateId, curr), '');
 }
-
 
 const renderTemplate = (selector, data, template) => {
   return selector.innerHTML += showItems(data, template);
@@ -19,78 +14,94 @@ const renderTemplate = (selector, data, template) => {
 
 
 const init = () => {
-  collapseCategories();
   getMovieData();
+  catModule.controlCat();
 }
 
 
-const categoriesTemplate = (function () {
+const getCategoriesData = (function() {
   let oReq = new XMLHttpRequest();
-  oReq.addEventListener('load', function () {
+  oReq.addEventListener('load', function() {
     let jsonData = JSON.parse(this.responseText);
-    getFilter(jsonData.categories);
-    switchFilter(jsonData.categories);
+    const { categories } = jsonData;
+    getFilter(categories);
+    switchFilter(categories);
   });
-
-
-  const getFilter = (data) => {
-    const filters = document.querySelector('.left-nav__categories-list__filter');
-    const filterLists = document.querySelector('.left-nav__categories-list__filtered-list');
-    return renderTemplate(filters, data, 'left-nav__categories-list__template') && renderTemplate(filterLists, data[0].detail, 'left-nav__categories-list__filter-list__template');
-  }
-
-  const switchFilter = (data) => {
-    const categoriesFilterBtns = document.querySelector('.left-nav__categories-list__filter');
-    categoriesFilterBtns.addEventListener('click', e => {
-      const filterLists = document.querySelector('.left-nav__categories-list__filtered-list');
-      filterLists.innerHTML =
-        `
-        <template id="left-nav__categories-list__filter-list__template">
-          <li class="left-nav__categories-list__filtered__item">
-            <a class="filtered__item__link" href="#">{{name}}</a>
-          </li>
-        </template>
-        `
-      data.forEach((el, i) => {
-        if (e.target.innerText === el.key) {
-          return renderTemplate(filterLists, data[i].detail, 'left-nav__categories-list__filter-list__template')
-        }
-      })
-    })
-  }
   oReq.open("GET", "src/js/data.json")
   oReq.send();
 })();
 
 
+const getFilter = (data) => {
+  const filters = $qs('.left-nav__categories-list__filter');
+  const filterLists = $qs('.left-nav__categories-list__filtered-list');
+  return renderTemplate(filters, data, 'left-nav__categories-list__template') && renderTemplate(filterLists, data[0].detail, 'left-nav__categories-list__filter-list__template');
+}
 
-const collapseCategories = () => {
-  const categoriesNav = document.querySelector('.left-nav__categories');
-  const categoriesLink = document.querySelector('.left-nav__categories-link');
-  const categoriesBox = document.querySelector('.left-nav__categories-list');
+const switchFilter = (data) => {
+  const filterBtns = $qs('.left-nav__categories-list__filter');
 
-  $on(categoriesNav, 'pointerover', e => {
-    e.target === categoriesLink && categoriesBox.classList.add('active')
-  })
+  filterBtns.addEventListener('click', e => {
+    const filterLists = $qs('.left-nav__categories-list__filtered-list');
+    filterLists.innerHTML =
+      `
+    <template id="left-nav__categories-list__filter-list__template">
+      <li class="left-nav__categories-list__filtered__item">
+        <a class="filtered__item__link" href="#">{{name}}</a>
+      </li>
+    </template>
+    `
 
-  $on(categoriesNav, 'pointerleave', e => {
-    e.target === categoriesBox || categoriesNav && categoriesBox.classList.remove('active')
+    const filterByCategories = (obj) => {
+      return e.target.innerText === obj.key && obj.key
+    }
+
+    data.filter(filterByCategories).forEach(el => {
+      return renderTemplate(filterLists, el.detail, 'left-nav__categories-list__filter-list__template');
+    })
   })
 }
+
+
+
+let catModule = (function() {
+  const _expandCat = function() {
+    const [catNav, catLink, catBox] = [$qs('.left-nav__categories'), $qs('.left-nav__categories-link'), $qs('.left-nav__categories-list')];
+    return $on(catNav, 'pointerover', e => { e.target === catLink && catBox.classList.add('active'); })
+  }
+
+  const _collapseCat = function() {
+    const [catNav, catBox] = [$qs('.left-nav__categories'), $qs('.left-nav__categories-list')];
+    return $on(catNav, 'pointerleave', e => { if (e.target === catBox || catNav) catBox.classList.remove('active'); })
+  }
+
+  return {
+    controlCat: function() {
+      let expand = _expandCat();
+      let collapse = _collapseCat();
+      return expand && collapse;
+    }
+  }
+})();
+
+
+
 
 
 
 const API = {
   state: 'https://api.themoviedb.org/3/discover/movie',
   api: '?api_key=64391ca210dbae0d44b0a622177ef8d3',
-  korean: '&language=ko',
+  korean: '&language=ko'
 }
+
+
 
 const getMovieData = () => {
   const { state, api, korean } = API;
   const movieDB = state + api + korean;
   let oReq = new XMLHttpRequest();
-  oReq.addEventListener('load', function () {
+  oReq.addEventListener('load', function() {
     let JSONmovieData = JSON.parse(this.responseText).results;
     let movieList = JSONmovieData;
     let movieSlideList = JSONmovieData.slice().sort(() => Math.random() - 0.5).slice(0, 3);
@@ -104,8 +115,8 @@ const getMovieData = () => {
 
 
 const renderContents = (movieData, slideData) => {
-  const movieContents = document.querySelector('.main__cinemas__list__body__slider__contents');
-  const sliderContent = document.querySelector('.main__slider__content');
+  const movieContents = $qs('.main__cinemas__list__body__slider__contents');
+  const sliderContent = $qs('.main__slider__content');
   renderTemplate(movieContents, movieData, 'main__cinemas__list__body__slider__contents__template');
   renderTemplate(sliderContent, slideData, 'content__template');
 }
@@ -113,7 +124,6 @@ const renderContents = (movieData, slideData) => {
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM Loaded");
   init();
 })
 
@@ -127,13 +137,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const swipeMainContents = () => {
   let count = 0;
-  const mainSlides = document.querySelectorAll(".content__container");
-  const prev = document.querySelector('.nav__prev__arrow-icon');
-  const next = document.querySelector('.nav__next__arrow-icon');
+  const mainSlides = $qsa(".content__container");
+  const prev = $qs('.nav__prev__arrow-icon');
+  const next = $qs('.nav__next__arrow-icon');
   mainSlides[0].style.opacity = '1';
-  const dots = document.querySelector('.nav__dots');
+  const dots = $qs('.nav__dots');
   const dotBtn = dots.querySelectorAll('.nav__dots__item')
   dotBtn[0].style.background = '#fff';
+
 
   prev.addEventListener('click', () => {
     if (count < mainSlides.length && count > 0) {
@@ -151,6 +162,8 @@ const swipeMainContents = () => {
       dotBtn[count].style.background = '#fff'
     }
   })
+
+
   next.addEventListener('click', () => {
     if (count < mainSlides.length - 1) {
       count++;
@@ -168,6 +181,7 @@ const swipeMainContents = () => {
     }
   })
 }
+
 
 const bindSlideBtn = (handler) => {
   return document.addEventListener('click', handler);
